cf79a1f4122431cd350af3cf70c46ba6178ef9c6
Naven2426 <112463079+Naven2426@users.noreply.github.com>
2023-10-20 08:33:22 +0530

    Create 4.	Write a C program to check whether a given string belongs to the language defined by a Context Free Grammar (CFG) S → 0S0 | 1S1 | 0 | 1 | ε .c

---
+++ 4.	Write a C program to check whether a given string belongs to the language defined by a Context Free Grammar (CFG) S → 0S0 | 1S1 | 0 | 1 | ε .c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+
+int S(char* str);
+
+int main() {
+    char input[100];
+    
+    printf("Enter a string: ");
+    scanf("%s", input);
+
+    if (S(input) == 1) {
+        printf("The string is in the language defined by the CFG.\n");
+    } else {
+        printf("The string is not in the language defined by the CFG.\n");
+    }
+    
+    return 0;
+}
+
+int S(char* str) {
+    if (str[0] == '0' && str[strlen(str) - 1] == '0') {
+        if (strlen(str) == 1) {
+            return 1; // Matches '0'
+        } else {
+            return S(str + 1); // Check the inner part
+        }
+    } else if (str[0] == '1' && str[strlen(str) - 1] == '1') {
+        if (strlen(str) == 1) {
+            return 1; // Matches '1'
+        } else {
+            return S(str + 1); // Check the inner part
+        }
+    } else if (strlen(str) == 0) {
+        return 1; // Empty string (epsilon)
+    }
+    return 0; // Not part of the language
+}
